7ca76db572ddef74db8ea1198cf258107ddecf75
Scott Long <smxlong@gmail.com>
2020-04-27 02:45:57 +0000

    enable private ip

--- create-nodes.sh
+++ create-nodes.sh
@@ -12,7 +12,7 @@ CLUSTER_NAME=dev-minecraft
 ENVIRONMENT=dev
 
 lc() {
-  linode-cli linodes create --root_pass bXuK86wKv4JXGP0gE5kbGYlHZiSG9c2w/MWH9jzd5Cg= --authorized_keys "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA4A+ki6PaU+SIYpkx5Cm6LtA0Lr05qGstQp2bySh9YU1wodRmcolRIbiaPzfalURrNcFjFfnWW2SuoyF4U3XeA7LCfrnN7T2d0Kp9facA4rSkSW7tSdZIgyo9trRK2d+lgGrr2Hs8wYAaU4D61ptksdVWfgpuPDXDdk90VWVAhP2FeB+EFB7CZxtw7p6TN4YC2GNpBQIPfvGvgnl/9WyrHQU2hMIWMQ9+2XmwMBF9iwpbCAkLhdNUNYrDI6sig9cLIAClvJLCgmHruSy1Ns2FjYbt9avpNulRZu6yBApJGzbF8thBYE0fgOB3gsHLqk+gjR7hGC6ZF68cyUfQIsjghw== rsa-key-20191220" $*
+  linode-cli linodes create --root_pass bXuK86wKv4JXGP0gE5kbGYlHZiSG9c2w/MWH9jzd5Cg= --authorized_keys "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEA4A+ki6PaU+SIYpkx5Cm6LtA0Lr05qGstQp2bySh9YU1wodRmcolRIbiaPzfalURrNcFjFfnWW2SuoyF4U3XeA7LCfrnN7T2d0Kp9facA4rSkSW7tSdZIgyo9trRK2d+lgGrr2Hs8wYAaU4D61ptksdVWfgpuPDXDdk90VWVAhP2FeB+EFB7CZxtw7p6TN4YC2GNpBQIPfvGvgnl/9WyrHQU2hMIWMQ9+2XmwMBF9iwpbCAkLhdNUNYrDI6sig9cLIAClvJLCgmHruSy1Ns2FjYbt9avpNulRZu6yBApJGzbF8thBYE0fgOB3gsHLqk+gjR7hGC6ZF68cyUfQIsjghw== rsa-key-20191220" --private_ip true $*
 }
 
 for N in $(seq $MASTER_COUNT); do
